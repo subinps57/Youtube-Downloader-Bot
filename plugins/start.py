52c8c9fd7a2f20ef075324a8dc6cae815f58a499
subinps57 <76514033+subinps57@users.noreply.github.com>
2020-12-23 19:34:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/subinps")]
     ])
-    welcomed = f"ഹലോ മിസ്റ്റർ <b>{message.from_user.first_name}</b>\nകൂടുതൽ അറിയാനും സഹായങ്ങൾകും/help ഉപയോഗിക്കുക "
+    welcomed = f"ഹലോ മിസ്റ്റർ <b>{message.from_user.first_name}</b>\nഞാൻ ആണ് SL8 Bot\nകൂടുതൽ അറിയാനും സഹായങ്ങൾകും/help ഉപയോഗിക്കുക "
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
